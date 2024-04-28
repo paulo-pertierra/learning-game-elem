acfbb0c21d23a03257b827e9f594d4cdbb8cb58b
Paulo Pertierra <paulopertierra@gmail.com>
2024-04-28 23:02:28 +0800

    feat: add videos table migration

---
+++ database/migrations/2024_04_28_145747_create_videos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('link');
+            $table->string('title');
+            $table->string('description');
+            $table->integer('grade_level');
+            $table->integer('quarter');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videos');
+    }
+};

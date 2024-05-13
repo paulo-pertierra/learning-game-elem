2111a45c63c31454b9c73635c452d45c326854e0
Paulo Pertierra <paulopertierra@gmail.com>
2024-05-13 18:00:53 +0800

    fix: reloading on upload

--- resources/js/Pages/Admin/Games.vue
+++ resources/js/Pages/Admin/Games.vue
@@ -5,7 +5,33 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue"
 <template>
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Games</h2>
+            <div class="flex items-center justify-between">
+                    <h2 class="font-semibold mr-2 text-xl text-gray-800 dark:text-gray-200 leading-tight">Games</h2>
+                <form class="flex items-center gap-4 ml-2" action="">
+                    <select
+                        class="w-fit g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        name="quarter" id="">
+                        <option value="" selected>Select a Quarter</option>
+                        <option value="1">1st Quarter</option>
+                        <option value="2">2nd Quarter</option>
+                        <option value="3">3rd Quarter</option>
+                        <option value="4">4th Quarter</option>
+                    </select>
+                    <select
+                        class="w-fit g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        name="quarter" id="">
+                        <option value="" selected>Select a Grade Level</option>
+                        <option value="1">Grade 1</option>
+                        <option value="2">Grade 2</option>
+                        <option value="3">Grade 3</option>
+                        <option value="4">Grade 4</option>
+                    </select>
+                    <button
+                        class="w-fit g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    >Filter
+                    </button>
+                    </form>
+                </div>
         </template>
     </AuthenticatedLayout>
 </template>
--- resources/js/Pages/Admin/Videos.vue
+++ resources/js/Pages/Admin/Videos.vue
@@ -47,10 +47,36 @@ const addNewVideo = () => {
 
 </script>
 <template>
+
     <Head title="Videos List" />
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Videos</h2>
+            <div class="flex items-center justify-between">
+                <h2 class="font-semibold mr-2 text-xl text-gray-800 dark:text-gray-200 leading-tight">Videos</h2>
+                <form class="flex items-center gap-4 ml-2" action="">
+                    <select
+                        class="w-fit g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        name="quarter" id="">
+                        <option value="" selected>Select a Quarter</option>
+                        <option value="1">1st Quarter</option>
+                        <option value="2">2nd Quarter</option>
+                        <option value="3">3rd Quarter</option>
+                        <option value="4">4th Quarter</option>
+                    </select>
+                    <select
+                        class="w-fit g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        name="quarter" id="">
+                        <option value="" selected>Select a Grade Level</option>
+                        <option value="1">Grade 1</option>
+                        <option value="2">Grade 2</option>
+                        <option value="3">Grade 3</option>
+                        <option value="4">Grade 4</option>
+                    </select>
+                    <button
+                        class="w-fit g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">Filter
+                    </button>
+                </form>
+            </div>
         </template>
 
         <div class="py-12">
@@ -71,10 +97,11 @@ const addNewVideo = () => {
                     <div class="text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                         <Link :href="`/videos/${video.id}`" v-for="video in videos.data"
                             class="border border-gray-500 dark:text-gray-200 aspect-video rounded-2xl flex items-end relative overflow-hidden">
-                            <img :src="`https://img.youtube.com/vi/${video.link}/0.jpg`" class="object-cover h-full w-full border brightness-75" alt="">
-                            <div class="p-4 absolute bg-black/75 w-full">
-                                <h2 class="text-2xl">{{ video.title }}</h2>
-                            </div>
+                        <img :src="`https://img.youtube.com/vi/${video.link}/0.jpg`"
+                            class="object-cover h-full w-full border brightness-75" alt="">
+                        <div class="p-4 absolute bg-black/75 w-full">
+                            <h2 class="text-2xl">{{ video.title }}</h2>
+                        </div>
                         </Link>
                         <div
                             class="md:col-span-2 lg:col-span-3 grid grid-cols-5 gap-2 mx-auto items-center justify-center">
